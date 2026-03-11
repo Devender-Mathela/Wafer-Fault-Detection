6e6e77b28b4dc578b4028d6373336f4783e4e827
Devender Mathela <deepunmt01@gmail.com>
2026-03-11 23:30:33 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Devender_Mathela",
+    author_email="deepnmt01@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
